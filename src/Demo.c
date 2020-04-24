c5ad6cc8e47d836ee460a55f0fefc033b56c39b5
John MacGregor <jmacgregor@pillartechnology.com>
2020-04-24 15:31:00 -0400

    Strip out first pass at filter stuff

--- src/Demo.c
+++ src/Demo.c
@@ -1,9 +1,7 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 #include <avr/sleep.h>
-#include <time.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 void led_init()
 {
@@ -29,53 +27,15 @@ void button_init()
     EIMSK |= (1 << INT0); // enable
 }
 
-volatile time_t last_interrupt;
-volatile time_t current_time;
-
-// volatile int last_interrupt;
-// volatile int current_time;
-
-void update_current_time()
-{
-    current_time = time(NULL);
-    // current_time = micros();
-}
-
-void update_interrupt_time()
-{
-    last_interrupt = time(NULL);
-    // last_interrupt = micros();
-}
-
-long interrupt_minimum_in_ms = 100;
-
 ISR (INT0_vect)
 {
-    // get time of interrupt
-    update_current_time();
-    // compare to "lastInterrupt"
-    long difference = current_time * 1000 - last_interrupt * 1000;
-    //int difference = current_time - last_interrupt;
-
-    if (difference > interrupt_minimum_in_ms)
-    {
-        update_interrupt_time(); // should we get time(NULL) again?
-        led_toggle();
-    }
-    else 
-    {
-        // ignore that bouncey clown
-    }
-
-    //led_toggle();
+    led_toggle();
 }
 
 int main(void) 
 {   
     led_init();
     button_init();
-    // set time of "lastInterrupt" as baseline
-    update_interrupt_time();
     sei(); // enable global interrupt
     
     set_sleep_mode(SLEEP_MODE_STANDBY);
